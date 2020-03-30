a497da2953590f94c1090f201df0f369e4f7fdf2
Lisandro <lisandroperezmarquez@gmail.com>
2020-03-30 15:45:15 -0600

    commit 1.0

---
+++ App SmartCaffe/src/vista/VentanaPrincipal.java
@@ -0,0 +1,154 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Frame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JRadioButton;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.BevelBorder;
+
+public class VentanaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPrincipal() {
+		setTitle("App Cafetería");
+		setExtendedState(Frame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 695, 358);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBorder(new EmptyBorder(0, 0, 0, 0));
+		menuBar.setBorderPainted(false);
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Control de Inventario");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("Almacén");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Control ");
+		menuBar.add(mnNewMenu_2);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{685, 0};
+		gbl_panel.rowHeights = new int[]{150, 0};
+		gbl_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/recursos/logo.png")));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 0;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/recursos/FondoBarraSuperior.png")));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.NORTH;
+		gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JPanel panelIzquierda = new JPanel();
+		panelIzquierda.setBorder(new TitledBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), "TIPO", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		contentPane.add(panelIzquierda, BorderLayout.WEST);
+		panelIzquierda.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Tostado y Molido");
+		panelIzquierda.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Tostado en Grano");
+		panelIzquierda.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Soluble");
+		panelIzquierda.add(rdbtnNewRadioButton_3);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Cápsulas");
+		panelIzquierda.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("Tés e infusiones");
+		panelIzquierda.add(rdbtnNewRadioButton_4);
+		
+		JRadioButton rdbtnNewRadioButton_6 = new JRadioButton("Promociones");
+		panelIzquierda.add(rdbtnNewRadioButton_6);
+		
+		JRadioButton rdbtnNewRadioButton_5 = new JRadioButton("Souvenirs");
+		panelIzquierda.add(rdbtnNewRadioButton_5);
+		
+		JPanel panelDerecha = new JPanel();
+		contentPane.add(panelDerecha, BorderLayout.EAST);
+		
+		JPanel panelInferior = new JPanel();
+		contentPane.add(panelInferior, BorderLayout.SOUTH);
+		
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
