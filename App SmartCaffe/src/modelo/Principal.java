7c18a59504e51a3f77ae3b51124f8fc00154562b
Λύσανδρος <30701863+freakymarlin@users.noreply.github.com>
2020-03-30 15:48:18 -0600

    Update Principal.java

--- App SmartCaffe/src/modelo/Principal.java
+++ App SmartCaffe/src/modelo/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Ah si se puede");
+		System.out.println("Lo que no se muy bien es como hacerlo desde eclipse");
 	}
 }
